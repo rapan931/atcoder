31444b647be239f54e29171e111f543b3dd6845e
rapan931 <24415677+rapan931@users.noreply.github.com>
2023-03-13 09:26:49 +0900

    abc293 a,b

---
+++ _result/_abc293/a/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	s := []rune(scans())
+	l := len(s)
+	for i := 0; i < l/2; i++ {
+		s[2*i], s[2*i+1] = s[2*i+1], s[2*i]
+	}
+	fmt.Println(string(s))
+}
+
+func init() {
+	buf := make([]byte, 10*1024)
+	sc.Buffer(buf, math.MaxInt32)
+
+	sc.Split(bufio.ScanWords)
+}
+
+func scan() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func scans() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func scanb() []byte {
+	sc.Scan()
+	return sc.Bytes()
+}
+
+func printSliceSepSpace(nums []int) {
+	fmt.Println(strings.Trim(fmt.Sprint(nums), "[]"))
+}
+
+type Queue struct{ v []int }
+
+func NewQueue() *Queue         { return &Queue{} }
+func (q *Queue) isEmpty() bool { return len(q.v) == 0 }
+func (q *Queue) len() int      { return len(q.v) }
+func (q *Queue) push(i int)    { q.v = append(q.v, i) }
+func (q *Queue) pop() int {
+	v := q.v[0]
+	q.v = q.v[1:]
+	return v
+}
+
+type Stack struct{ v []int }
+
+func NewStack(n int) *Stack    { return &Stack{} }
+func (s *Stack) isEmpty() bool { return len(s.v) == 0 }
+func (s *Stack) len() int      { return len(s.v) }
+func (s *Stack) push(i int)    { s.v = append(s.v, i) }
+func (s *Stack) pop() int {
+	v := s.v[len(s.v)-1]
+	s.v = s.v[:len(s.v) - 1]
+	return v
+}
